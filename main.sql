e065cfad61939897cb7f1f08da0b9fb16349c24c
Jupiter Debbarma <49095440+jupiterdv@users.noreply.github.com>
2022-02-14 02:08:16 +0530

    Update main.sql

--- main.sql
+++ main.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `u995265517_jupiter`
+-- Database: `@yourdatabase`
 --
 
 -- --------------------------------------------------------
